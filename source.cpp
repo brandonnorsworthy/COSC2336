fdc362690db0c52054188a6d275a61a7c8cdddc9
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2019-09-10 20:32:29 -0500

    Cleaned up source.cpp removed cin, cout
    Modified and added some comments clarifiying some variables and steps in the cpp file.

--- source.cpp
+++ source.cpp
@@ -11,24 +11,18 @@ September 10, 2019
 int vowels(char* cstring);
 
 int main() {
-	//input file
-	std::ifstream iFile("input.txt");
-	std::ofstream oFile("output.txt");
-
-	//test input string to verify vowelFinder works
-	std::string str;
-	int vowelAmount = 0;
+	std::ifstream iFile("input.txt"); //input file
+	std::ofstream oFile("output.txt"); //output file
+	std::string str; //string to copy text into until its converted to a cstring
+	int vowelAmount = 0; //stores amount of vowels while going through the lines
 
 	while (std::getline(iFile, str))
 	{
-		//read input std::cin tests input iFile tests txt
-		//std::getline(iFile, str); //make sure to grab incase multiple words/spaces present
-
-		//copies over str int a cstring to send off to vowelFinder
+		//Copies the input string over to a cstring to send off to vowel counter
 		char* cstr = new char[str.length() + 1];
 		strcpy_s(cstr, str.length() + 1, str.c_str());
 
-		//loops through cstr to test for spaces and convert to uppercase
+		//loops through to make all alphabetical characters uppercase so if statements are reduced
 		char c;
 		for (int i = 0; i < strlen(cstr); i++)
 		{
@@ -38,7 +32,6 @@ int main() {
 				if (!isspace(c))
 				{
 					cstr[i] = toupper(cstr[i]);
-					//std::cout<< cstr[i] << ", "; //prints out characters to make sure they are uppercase
 				}
 			}
 		}
@@ -48,7 +41,6 @@ int main() {
 
 	char vowelAmountC[10];
 	_itoa_s(vowelAmount, vowelAmountC, 10);
-	std::cout << "Vowel amount: " << vowelAmountC;
 	if (oFile)
 	{
 		oFile << vowelAmountC;
@@ -64,7 +56,6 @@ int vowels(char* cstring)
 	//if cstring is empty stop recursion
 	if (cstring[0] == NULL)
 	{
-		//std::cout << "cstring == null" << std::endl;
 		return 0;
 	}
 	else
@@ -72,13 +63,11 @@ int vowels(char* cstring)
 		//test if current char at front of cstring is a vowel if it is add 1 to vowel count and call function again
 		if (cstring[0] == 'A' || cstring[0] == 'E' || cstring[0] == 'I' || cstring[0] == 'O' || cstring[0] == 'U')
 		{
-			//std::cout << "cstring[0] = " << cstring[0] << std::endl; //prints out front character to make sure its a vowel
 			memmove(&cstring[0], &cstring[1], strlen(cstring)); //remove front character after it is tested
 			return 1 + vowels(cstring);
 		}
 		else
 		{
-			//std::cout << "cstring[0] = " << cstring[0] << std::endl; //prints out front character to make sure its not a vowel
 			memmove(&cstring[0], &cstring[1], strlen(cstring)); //remove front character after it is tested
 			return 0 + vowels(cstring);
 		}
